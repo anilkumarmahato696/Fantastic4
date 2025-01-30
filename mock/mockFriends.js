c272321423d37a440a2d0096825a26ddecfb30c2
Anil <anilmahato712@gmail.com>
2025-01-30 18:58:26 +0100

    Updates

---
+++ mock/mockFriends.js
@@ -0,0 +1,10 @@
+export const mockFriends = [{
+    name: "Anil Kumar",
+    img: "avatar1.png"
+},{
+    name: "Yusufu Tureke",
+    img: "avatar2.png"
+},{
+    name: "Vinay Kumar",
+    img: "avatar3.png"
+}]
---
+++ mock/mockTrends.js
@@ -0,0 +1,14 @@
+export const mockTrends = [
+  {
+    title: "Nepal",
+    posts: 19000,
+  },
+  {
+    title: "China",
+    posts: 15000,
+  },
+  {
+    title: "India",
+    posts: 10000,
+  },
+];
